98f7c755607d6fe722d7f9a1a9d6992a6ffc1cec
Yogi1710 <103318661+yogi1710@users.noreply.github.com>
2023-04-28 11:06:20 +0530

    Update and rename queries on rgm1 table.sql to where queries on rgm1 table.sql

--- where queries on rgm1 table.sql
+++ where queries on rgm1 table.sql
@@ -9,3 +9,6 @@ select * from rgm1 where NAME_ like "S%";-- % means ,whatever may be
 select * from rgm1 where NAME_ like "SA%";
 select * from rgm1 where ADDRESS like "%L";
 select * from rgm1 where ADDRESS like "%oo%";
+select * from rgm1 where NAME_ like "_M%";-- _ means it might be any single character in the underscore position 
+select * from rgm1 where NAME_ not like "s%";-- will not show the names which are not starting with s (we use not like)
+select * from rgm1 where NAME_ not like "_M%";
